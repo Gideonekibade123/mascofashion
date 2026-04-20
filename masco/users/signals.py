00bd45f339ca49d457261eb330286af438260dc2
Gideon <ekibade.gideon@yahoo.comm>
2026-04-20 21:07:21 +0100

    Updating my signal because of my reset password

--- masco/users/signals.py
+++ masco/users/signals.py
@@ -1,3 +1,27 @@
+# from django.dispatch import receiver
+# from django.core.mail import send_mail
+# from django.conf import settings
+# from django_rest_passwordreset.signals import reset_password_token_created
+
+
+# @receiver(reset_password_token_created)
+# def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+#     email_plaintext_message = f"""
+# Use the link below to reset your password:
+
+# http://localhost:5173/reset-password/{reset_password_token.key}
+# """
+
+#     send_mail(
+#         "Password Reset for Masco Fashion",
+#         email_plaintext_message,
+#         settings.DEFAULT_FROM_EMAIL,
+#         [reset_password_token.user.email],
+#     )
+
+
+
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
@@ -10,7 +34,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     email_plaintext_message = f"""
 Use the link below to reset your password:
 
-http://localhost:5173/reset-password/{reset_password_token.key}
+{settings.FRONTEND_URL}/reset-password/{reset_password_token.key}
 """
 
     send_mail(
